ea3433cea2bf4debd59eede22825d6ff4284b909
shachi <52762781+shachy@users.noreply.github.com>
2021-02-12 16:44:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from distutils.core import setup
 
 setup(
-  name = 'Vec',         # How you named your package folder (MyLib)
-  packages = ['Vec'],   # Chose the same as "name"
+  name = 'Ord',         # How you named your package folder (MyLib)
+  packages = ['Ord'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package to help me and maybe other people use more efficient lists',   # Give a short description about your library
   author = 'shachy',                   # Type in your name
   author_email = 'shachar262@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/shachy/Vec',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/shachy/Vec/archive/0.1.zip',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  download_url = 'https://github.com/shachy/Ord/archive/0.1.zip',    # I explain this later on
+  keywords = ['list', 'vector', 'order'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'random'
+          'random',
+
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
