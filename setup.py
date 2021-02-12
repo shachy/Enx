c89f63946bb7044a65309d72b767e80aca1aab32
shachi <52762781+shachy@users.noreply.github.com>
2021-02-12 16:32:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
   download_url = 'https://github.com/shachy/Vec/archive/0.1.zip',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'random'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
