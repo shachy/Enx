93bbba905e3d585c804cb4f9ab42b7abdcdc2328
shachi <52762781+shachy@users.noreply.github.com>
2021-02-12 16:22:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'shachy',                   # Type in your name
   author_email = 'shachar262@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/shachy/Vec',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/shachy/Vec/archive/0.1.zip',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
